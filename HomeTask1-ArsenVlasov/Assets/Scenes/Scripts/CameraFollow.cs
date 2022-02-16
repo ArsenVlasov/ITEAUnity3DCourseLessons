a4c567eb4626a48790d91a54e18661683063184e
Arsen <arsenvlasov2001@gmail.com>
2022-02-16 22:14:06 +0200

    Added HomeTask1-ArsenVlasov project

---
+++ HomeTask1-ArsenVlasov/Assets/Scenes/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float smoothSpeed = 0.125f;
+    [SerializeField] public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ HomeTask1-ArsenVlasov/Assets/Scenes/Scripts/WizardMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class WizardMovement : MonoBehaviour
+{
+    [SerializeField] public float speed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Movement(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Movement(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Movement(Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Movement(Vector3.right);
+        }
+    }
+
+    private void Movement(Vector3 direction)
+    { 
+        transform.position = transform.position + direction*speed*Time.deltaTime;
+    }
+}

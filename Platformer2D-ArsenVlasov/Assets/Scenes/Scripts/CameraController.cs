f4b66db0313cf42281416b54390aae6ba1ba9d8f
Arsen <arsenvlasov2001@gmail.com>
2022-02-27 00:45:38 +0200

    Added movement, camera control and level

---
+++ Platformer2D-ArsenVlasov/Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] public GameObject player;
+    [SerializeField] public float leftLimit;
+    [SerializeField] public float rightLimit;
+    [SerializeField] public float topLimit;
+    [SerializeField] public float bottomLimit;
+
+    [SerializeField] public bool isLimitCameraWork = true;
+
+    public void Update()
+    {
+        FollowToPlayer();
+        if (isLimitCameraWork)
+        {
+            LimitCamera();
+        }
+    }
+
+    private void FollowToPlayer()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -1f);
+    }
+
+    private void LimitCamera()
+    {
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x , leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            -1f
+            );
+    }
+
+    private void OnDrawGizmos()
+    { 
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+    }
+}

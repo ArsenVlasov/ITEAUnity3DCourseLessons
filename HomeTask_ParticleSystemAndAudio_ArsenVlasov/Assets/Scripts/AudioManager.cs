660d6795d51d4d04e921e847db1e66a9cbf25250
ArsenVlasov <arsenvlasov2001@gmail.com>
2022-05-19 00:37:55 +0300

    Added ParticleSystemAndAudio hometask

---
+++ HomeTask_ParticleSystemAndAudio_ArsenVlasov/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource _fireworkSource;
+    [SerializeField] private List<FireworkMusicData> _musicData = new List<FireworkMusicData>();
+
+    private static AudioManager _instance;
+
+    public enum FireworkSoundType
+    {
+        Blue,
+        Burst,
+        CampFireClap,
+        Deep,
+        HeavyShot,
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void PlayMusicInner(FireworkSoundType fireworkSoundType)
+    {
+        var musicData = _musicData.Find(data => data.SoundType == fireworkSoundType);
+        if (musicData != null)
+        {
+            _fireworkSource.PlayOneShot(musicData.Audio);
+        }
+    }
+
+    public static void PlayMusic(FireworkSoundType fireworkSound)
+    {
+        _instance.PlayMusicInner(fireworkSound);
+    }
+
+
+    [System.Serializable]
+    public class FireworkMusicData
+    {
+        [SerializeField] private FireworkSoundType _fireworkSoundType;
+        [SerializeField] private AudioClip _audioClip;
+
+        public FireworkSoundType SoundType => _fireworkSoundType;
+        public AudioClip Audio => _audioClip;
+    }
+}
---
+++ HomeTask_ParticleSystemAndAudio_ArsenVlasov/Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+public class GameController : MonoBehaviour
+{
+    //[SerializeField] private ParticleSystem _parentParticleSystem;
+    [SerializeField] private AudioManager.FireworkSoundType _soundType;
+
+    //private int _currentNumberOfParticles = 0;
+    [SerializeField] private int coroutineWait = 1;
+    bool keepPlaying = true;
+
+    void Start()
+    {
+        StartCoroutine(SoundOut());
+    }
+
+    IEnumerator SoundOut()
+    {
+        while (keepPlaying)
+        {
+            PlayFireworkShot();
+            yield return new WaitForSeconds(coroutineWait);
+        }
+    }
+
+    /*void Start()
+    {
+        *//*_parentParticleSystem = this.GetComponent<ParticleSystem>();
+        if (_parentParticleSystem == null)
+            Debug.LogError("Missing ParticleSystem!", this);*//*
+    }*/
+
+    void Update()
+    {
+        /*var amount = Mathf.Abs(_currentNumberOfParticles - _parentParticleSystem.particleCount);
+
+        if (_parentParticleSystem.particleCount < _currentNumberOfParticles)
+        {
+            AudioManager.PlayMusic(_soundType);
+        }*/
+        //PlayFireworkShot();
+
+    }
+
+    private void PlayFireworkShot()
+    {
+        AudioManager.PlayMusic(_soundType);
+    }
+}
